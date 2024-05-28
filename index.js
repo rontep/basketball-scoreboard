387252d881a0e679beb061ac7032b005db210443
Ron Tep <ronaldtep+github@gmail.com>
2024-05-28 10:55:32 -0700

    Edit the pointAdd function to reduce repeated code

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-// Declare variables for home and guest points
-let homePoints = document.getElementById("home-points")
-let guestPoints = document.getElementById("guest-points")
-
-// Define functions for adding points 
-function plusOne(team) {
-    if (team === "home") {
-        let sum = parseInt(homePoints.textContent) + 1
-        homePoints.textContent = sum
-    } else if (team === "guest") {
-        let sum = parseInt(guestPoints.textContent) + 1
-        guestPoints.textContent = sum
-    }
-}
-
-function plusTwo(team) {
-    if (team === "home") {
-        let sum = parseInt(homePoints.textContent) + 2
-        homePoints.textContent = sum
-    } else if (team === "guest") {
-        let sum = parseInt(guestPoints.textContent) + 2
-        guestPoints.textContent = sum
-    }
-}
-
-function plusThree(team) {
-    if (team === "home") {
-        let sum = parseInt(homePoints.textContent) + 3
-        homePoints.textContent = sum
-    } else if (team === "guest") {
-        let sum = parseInt(guestPoints.textContent) + 3
-        guestPoints.textContent = sum
-    }
+// Define function for adding points to correct team
+function plusPoints(team, toAdd) {
+    let teamPoints = document.getElementById(team)
+    let sum = parseInt(teamPoints.textContent) + toAdd
+    teamPoints.textContent = sum
 }
